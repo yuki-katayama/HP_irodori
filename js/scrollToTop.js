373ea764e70bad9cc1a8db6b8c68517fdf233e46
yuki-katayama <katayu810@gmail.com>
2022-12-31 15:07:01 +0900

    scroll of top

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,4 +1,8 @@
 function scrollToTop() {
 	let speed = 500;
 	jQuery("html, body").animate({scrollTop:0}, speed, "swing");
+	jQuery("header").animate({scrollTop:0}, speed, "swing");
 }
+
+
+// 免許証、保険証
