cbe772d317d5da6bfafb44b0b3c1ec515b5357f2
yuki-katayama <katayu810@gmail.com>
2022-12-31 13:41:30 +0900

    微調整

--- js/scrollListener.js
+++ js/scrollListener.js
@@ -2,7 +2,9 @@ document.addEventListener('scroll', function (e) {
 	const scrollToTop = document.getElementById("scroll-to-top");
 	if (window.scrollY < 350) {
 		scrollToTop.style.opacity = 0;
+		scrollToTop.style['pointer-events'] = 'none';
 	} else {
+		scrollToTop.style['pointer-events'] = 'unset';
 		scrollToTop.style.opacity = 1;
 	}
 })
